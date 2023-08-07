8f569762d183577c0d9b28162f40cb74ec940136
Gomson-E <gomsonixx@gmail.com>
2023-08-07 21:53:34 +0900

    FEAT : myConfirm 기능

--- indicators.js
+++ indicators.js
@@ -8,7 +8,7 @@ function myAlert(content) {
                             <span aria-hidden="true"></span>
                         </button>
                         </div>
-                        <div class="toast-body" style="padding: 20px; font-size: 17px">
+                        <div class="toast-body">
                         ${content}
                         </div>
                     </div>
@@ -19,6 +19,40 @@ function myAlert(content) {
 
     $("#alert #closeBtn").click(function() {
         removeIndicator("alert")
+    })
+}
+
+function myConfirm(content) {
+    var confirm = `<div id="confirm" style="position: fixed;">
+                        <div class="toast show" role="alert" aria-live="assertive" aria-atomic="true" style="width: 450px; height: 200px">
+                            <div class="toast-header">
+                            <strong class="me-auto">Confirm</strong>
+                            <small></small>
+                            <button type="button" id="closeBtn" class="btn-close ms-2 mb-1" data-bs-dismiss="toast" aria-label="Close">
+                                <span aria-hidden="true"></span>
+                            </button>
+                            </div>
+                            <div class="toast-body">
+                            ${content}
+                            </div>
+                            <button type="button" id="confirmBtn" class="btn btn-primary" style="position: absolute; right : 110px; bottom: 10px">Confirm</button>
+                            <button type="button" id="closeBtn" class="btn btn-secondary" style="position: absolute; right : 10px; bottom: 10px" data-bs-dismiss="modal">Close</button>
+                        </div>
+                    </div>`
+    
+    addBlock()
+    $("body").append(confirm)
+
+    $("")
+
+    $("#confirm #confirmBtn").click(function() {
+        removeIndicator("confirm")
+        return true;
+    }) 
+
+    $("#confirm #closeBtn").click(function() {
+        removeIndicator("confirm")
+        return false;
     })
 }
 
